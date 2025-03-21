9cf1e9144c813b9e5c6dfff777dc81fd0b990eac
sangeethkumar.h2019 <sangeethkumar.h2019@vitalum.ac.in>
2025-03-21 19:11:24 +0000

    GTM dependency integrations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import 'zone.js';
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Idreams';
+
+  constructor(private gtmService: GoogleTagManagerService) {}
+
+  trackEvent(action: string, label: string) {
+    const gtmTag = {
+      event: 'user_action',
+      action: action,
+      label: label,
+    };
+    this.gtmService.pushTag(gtmTag);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms'; // Import ReactiveFormsModule
+import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app.component'; // Import your standalone component
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { firebaseConfig } from '../environments/environments';
-
+import { GoogleTagManagerModule } from 'angular-google-tag-manager';
+import { GtmService } from './gtm.service';
 @NgModule({
   imports: [
     BrowserModule,
+    CommonModule,
     ReactiveFormsModule, // Add ReactiveFormsModule here
-    AppComponent, // Import AppComponent here
+    GoogleTagManagerModule.forRoot({
+      id: 'AW-16827617620',
+    }),
   ],
-  providers: [],
+  bootstrap: [AppComponent],
+  providers: [{provide: 'googleTagManagerId', useValue:"AW-16827617620"}],
 })
-export class AppModule { 
-  
+export class AppModule {
 }
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
